63efefaa781c3194dd4197f9103e8c21e442da27
Misha Kharkov <Mikhail.Kharkov@dataart.com>
2016-07-05 15:41:26 +0300

    Revert "ubuntu commit"
    This reverts commit 44c569ce418df1d4f6263100fcec4706ea023564.

--- chat-protocol/src/main/java/practice/chat/protocol/shared/messages/Message.java
+++
@@ -1,9 +0,0 @@
-package practice.chat.protocol.shared.messages;
-
-import java.io.Serializable;
-
-/**
- * Created by misha on 20.05.16.
- */
-public interface Message extends Serializable {
-}
--- chat-protocol/src/main/java/practice/chat/protocol/shared/messages/request/CreateRoom.java
+++
@@ -1,9 +0,0 @@
-package practice.chat.protocol.shared.messages.request;
-
-import practice.chat.protocol.shared.messages.Message;
-
-/**
- * Created by misha on 20.05.16.
- */
-public class CreateRoom implements Message {
-}
